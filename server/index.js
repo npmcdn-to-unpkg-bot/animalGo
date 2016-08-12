08369bd643d1a182ffa187fbf94bc618fbd38cb7
Katya Aluf <Katya Aluf>
2016-08-12 23:49:05 +0300

    server side searching for close users for action

--- server/index.js
+++ server/index.js
@@ -1,12 +1,58 @@
 var express = require('express');
 var app = express();
-var Firebase = require("firebase");
+var firebase = require("firebase");
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
-});
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
-});
+var config = {
+	apiKey: "AIzaSyBj4uirFNsfy_YDLOai-O3rvHM1XwzQ8wk",
+	authDomain: "roadtofreedom-aea63.firebaseapp.com",
+	databaseURL: "https://roadtofreedom-aea63.firebaseio.com",
+	storageBucket: "roadtofreedom-aea63.appspot.com",
+};
+firebase.initializeApp(config);
 
+var serverStart = new Date();
+
+
+var spottingsRef = firebase.database().ref('spottings');
+var usersRef = firebase.database().ref('users');
+spottingsRef.on('child_added', function(data) {
+
+        var now = new Date();
+        var delta = now - serverStart;
+
+		if (delta > 2000) {
+				console.log('val -> ', data.val());
+		}
+});// listening to changes
+
+function findCloseUsers(location) {
+    usersRef.once("value", function (snapshot) {
+        var users = snapshot.val();
+        Object.keys(users).forEach(function (userId) {
+            var user = users[userId];
+            console.log(user);
+
+        });
+    });
+}
+
+function newSpotting() {
+	spottingsRef.push().set({
+		description: "hello2"
+	});
+
+	findCloseUsers();
+}
+
+setTimeout(function () {
+    newSpotting();
+}, 3000);
+
+function writeSpotting(userId, name, email, imageUrl) {
+	firebase.database().ref('users/' + userId).set({
+		username: name,
+		email: email,
+		profile_picture : imageUrl
+	});
+}
